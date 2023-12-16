da9a3ca98c15ed160a05b3c8f6117b1066bbb797
BaptisteBrs <baptiste.boursin@gmail.com>
2023-12-16 15:49:44 +0100

    Update GameRepository.php

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -21,8 +21,8 @@ class GameRepository
 {
     public function all()
     {
-        dd(Auth::user()->is_a('admin'));
-        
+        return(Auth::user()->is_a('admin'));
+
         if (Auth::user()->is_a('admin')) {
             return Game::with('type', 'team.category')->orderBy('date', 'desc')->get();
         }else{
